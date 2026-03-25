a3a1a1b938450e39f81e9dfc0048b308718b49fe
23MIS0050 <kaarthik.m2023@vitstudent.ac.in>
2026-03-25 14:25:26 +0530

    I changed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,10 @@
-// package src.main.java;
-// public class App {
-//  public static void main(String[] args) throws Exception {
-//  System.out.println("Java CI/CD Application Started...");
-//  while (true) {
-//  Thread.sleep(5000);
-//  System.out.println("Hello from Java CI/CD Pipeline!");
-//  }
-//  }
-// }
-
 import java.io.OutputStream;
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
         server.createContext("/", exchange -> {
