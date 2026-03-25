a736d90c2ad47b7b9d517620d3397e864e2408bb
23MIS0050 <kaarthik.m2023@vitstudent.ac.in>
2026-03-25 14:18:12 +0530

    Changed Java File

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,33 @@
-package src.main.java;
+// package src.main.java;
+// public class App {
+//  public static void main(String[] args) throws Exception {
+//  System.out.println("Java CI/CD Application Started...");
+//  while (true) {
+//  Thread.sleep(5000);
+//  System.out.println("Hello from Java CI/CD Pipeline!");
+//  }
+//  }
+// }
+
+import java.io.OutputStream;
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+
 public class App {
- public static void main(String[] args) throws Exception {
- System.out.println("Java CI/CD Application Started...");
- while (true) {
- Thread.sleep(5000);
- System.out.println("Hello from Java CI/CD Pipeline!");
- }
- }
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "Hello from Java CI/CD Pipeline 🚀";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Server started at http://localhost:8080");
+    }
 }
